cf9ca3043b6e0964ca9401d32e0598f60d7a0e11
Manan Ghazaryan <ghazaryan17@yandex.ru>
2022-09-23 13:47:22 -0400

    lesson 4.3

--- my portfolio/js/index.js
+++ my portfolio/js/index.js
@@ -7,11 +7,42 @@ copyright.innerHTML = myName.concat('', thisYear);
 footer.appendChild(copyright); 
 
 let skills = ['html', 'css', 'js']; 
-let skillsSection = document.getElementById('.skills');
-let skillsList = document.querySelector('ul');
+let skillsSection = document.getElementById('skills');
+let skillsList = skillsSection.querySelector('ul');
 
 for (i=0; i<skills.length; i++){
     let skill = document.createElement('li');
     skill.innerHTML = skills[i];
     skillsList.appendChild(skill);
 }
+
+
+let messageForm = document.querySelector('[name = leave_message]')
+messageForm.addEventListener('submit',function(event){
+    event.preventDefault()
+
+    let name = event.target.name;
+    let email = event.target.email;
+    let message = event.target.message;
+    console.log(name);
+    console.log(email);
+    console.log(message);
+
+
+    let messageSection = document.getElementById('messages')
+    let messageList = messageSection.querySelector('ul')
+    let newMessage = document.createElement('li')
+    newMessage.innerHTML = '<a href="mailto:email">name</a><span>message</span>'
+    let removeButton = document.createElement('button')
+    removeButton.innerText = 'Remove'
+    removeButton.setAttribute('type', 'button') 
+    removeButton.addEventListener('click', function(event){
+    let entry = document.querySelector("button").parentNode;
+    entry.remove(); 
+    removeButton.appendChild(newMessage) 
+    newMessage.append(messageList) 
+})
+    document.querySelector('[name = leave_message]').reset();
+});  
+
+
